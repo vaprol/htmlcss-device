57459a4874553e85e5ae584becd9efd2c3274f96
Konstantin Pivnov <vaprol@gmail.com>
2019-08-01 12:45:07 +0300

    Добавил сложный фон в страницу каталога, ограничил лишние скрипты для страницы каталога

--- js/script.js
+++ js/script.js
@@ -28,84 +28,86 @@
     subMenu.classList.add('visually-hidden');
   });
 
-// Открытие-закрытие попапа карты
-  var onMapOpenerButtonClick = document.querySelector('.bigmap-open');
-  var mapPopup = document.querySelector('.map-popup');
-  var onMapCloseButtonClick = mapPopup.querySelector('.close-popup');
+  if (!document.querySelector('.catalog-page')) {
+    // Открытие-закрытие попапа карты
+    var onMapOpenerButtonClick = document.querySelector('.bigmap-open');
+    var mapPopup = document.querySelector('.map-popup');
+    var onMapCloseButtonClick = mapPopup.querySelector('.close-popup');
 
-  onMapOpenerButtonClick.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    mapPopup.classList.toggle('visually-hidden');
-  });
-  onMapCloseButtonClick.addEventListener('click', function () {
-    mapPopup.classList.add('visually-hidden');
-  });
+    onMapOpenerButtonClick.addEventListener('click', function (evt) {
+      evt.preventDefault();
+      mapPopup.classList.toggle('visually-hidden');
+    });
+    onMapCloseButtonClick.addEventListener('click', function () {
+      mapPopup.classList.add('visually-hidden');
+    });
 
 // Открытие-закрытие попапа обратной связи
-  var onFeedbackOpenerButtonClick = document.querySelector('.feedback-popup-opener');
-  var feedbackPopup = document.querySelector('.feedback-popup');
-  var onFeedbackCloseButtonClick = feedbackPopup.querySelector('.close-popup');
+    var onFeedbackOpenerButtonClick = document.querySelector('.feedback-popup-opener');
+    var feedbackPopup = document.querySelector('.feedback-popup');
+    var onFeedbackCloseButtonClick = feedbackPopup.querySelector('.close-popup');
 
-  onFeedbackOpenerButtonClick.addEventListener('click', function (evt) {
-    feedbackPopup.classList.toggle('visually-hidden');
-  });
-  onFeedbackCloseButtonClick.addEventListener('click', function () {
-    feedbackPopup.classList.add('visually-hidden');
-  });
+    onFeedbackOpenerButtonClick.addEventListener('click', function (evt) {
+      feedbackPopup.classList.toggle('visually-hidden');
+    });
+    onFeedbackCloseButtonClick.addEventListener('click', function () {
+      feedbackPopup.classList.add('visually-hidden');
+    });
 
 //  Работа со слайдером
-  var sliderButtonsNode = document.querySelector('.slider-handlers');
-  var sliderButtons = sliderButtonsNode.querySelectorAll('.slider-select');
-  var mainSlider = document.querySelector('.promo-slider');
-  var slides = mainSlider.querySelectorAll('.slider-list > li');
+    var sliderButtonsNode = document.querySelector('.slider-handlers');
+    var sliderButtons = sliderButtonsNode.querySelectorAll('.slider-select');
+    var mainSlider = document.querySelector('.promo-slider');
+    var slides = mainSlider.querySelectorAll('.slider-list > li');
 
-  var changeSlide = function () {
-    var currentSlide = mainSlider.querySelector('.slide-shown');
-    var currentSlideIndex = Array.from(slides).indexOf(currentSlide);
-    var nextSlideIndex = 0;
-    if ((currentSlideIndex + 1) < slides.length) {
-      nextSlideIndex = currentSlideIndex + 1;
-    }
-    currentSlide.classList.remove('slide-shown');
-    currentSlide.classList.add('slide-hidden');
-    slides[nextSlideIndex].classList.remove('slide-hidden');
-    slides[nextSlideIndex].classList.add('slide-shown');
-    sliderButtons[currentSlideIndex].classList.remove('slider-button-selected');
-    sliderButtons[nextSlideIndex].classList.add('slider-button-selected');
-  };
-  var sliderInterval = setInterval(changeSlide, 5000);
+    var changeSlide = function () {
+      var currentSlide = mainSlider.querySelector('.slide-shown');
+      var currentSlideIndex = Array.from(slides).indexOf(currentSlide);
+      var nextSlideIndex = 0;
+      if ((currentSlideIndex + 1) < slides.length) {
+        nextSlideIndex = currentSlideIndex + 1;
+      }
+      currentSlide.classList.remove('slide-shown');
+      currentSlide.classList.add('slide-hidden');
+      slides[nextSlideIndex].classList.remove('slide-hidden');
+      slides[nextSlideIndex].classList.add('slide-shown');
+      sliderButtons[currentSlideIndex].classList.remove('slider-button-selected');
+      sliderButtons[nextSlideIndex].classList.add('slider-button-selected');
+    };
+    var sliderInterval = setInterval(changeSlide, 5000);
 
-  var changeSlideManually = function (slideIndex) {
-    mainSlider.querySelector('.slide-shown').classList.add('slide-hidden');
-    mainSlider.querySelector('.slide-shown').classList.remove('slide-shown');
-    slides[slideIndex-1].classList.remove('slide-hidden');
-    slides[slideIndex-1].classList.add('slide-shown');
-    mainSlider.querySelector('.slider-button-selected').classList.remove('slider-button-selected');
-    sliderButtons[slideIndex-1].classList.add('slider-button-selected');
-  };
-  sliderButtonsNode.addEventListener('click', function (evt) {
-    if (evt.target.nodeName === 'BUTTON') {
-      clearInterval(sliderInterval);
-      changeSlideManually(Array.from(sliderButtons).indexOf(evt.target) + 1);
-    }
-  });
+    var changeSlideManually = function (slideIndex) {
+      mainSlider.querySelector('.slide-shown').classList.add('slide-hidden');
+      mainSlider.querySelector('.slide-shown').classList.remove('slide-shown');
+      slides[slideIndex - 1].classList.remove('slide-hidden');
+      slides[slideIndex - 1].classList.add('slide-shown');
+      mainSlider.querySelector('.slider-button-selected').classList.remove('slider-button-selected');
+      sliderButtons[slideIndex - 1].classList.add('slider-button-selected');
+    };
+    sliderButtonsNode.addEventListener('click', function (evt) {
+      if (evt.target.nodeName === 'BUTTON') {
+        clearInterval(sliderInterval);
+        changeSlideManually(Array.from(sliderButtons).indexOf(evt.target) + 1);
+      }
+    });
 
 //  Работа блока услуг
-  var serviceButtonsNode = document.querySelector('.service-categories');
-  var serviceDescriptionsNode = document.querySelector('.service-descriptions');
+    var serviceButtonsNode = document.querySelector('.service-categories');
+    var serviceDescriptionsNode = document.querySelector('.service-descriptions');
 
-  serviceButtonsNode.addEventListener('click', function (evt) {
-    if (evt.target.nodeName === 'BUTTON') {
-      var oldCategory = serviceButtonsNode.querySelector('button[disabled]').classList[1].split('choose-').join('');
-      var newCategory = evt.target.classList[1].split('choose-').join('');
+    serviceButtonsNode.addEventListener('click', function (evt) {
+      if (evt.target.nodeName === 'BUTTON') {
+        var oldCategory = serviceButtonsNode.querySelector('button[disabled]').classList[1].split('choose-').join('');
+        var newCategory = evt.target.classList[1].split('choose-').join('');
 
-      serviceButtonsNode.querySelector('button[disabled]').parentElement.classList.remove('active-category');
-      serviceButtonsNode.querySelector('button[disabled]').disabled = false;
-      serviceDescriptionsNode.querySelector('.description-' + oldCategory).classList.add('hidden');
+        serviceButtonsNode.querySelector('button[disabled]').parentElement.classList.remove('active-category');
+        serviceButtonsNode.querySelector('button[disabled]').disabled = false;
+        serviceDescriptionsNode.querySelector('.description-' + oldCategory).classList.add('hidden');
 
-      evt.target.disabled = true;
-      evt.target.parentElement.classList.add('active-category');
-      serviceDescriptionsNode.querySelector('.description-' + newCategory).classList.remove('hidden');
-    }
-  })
+        evt.target.disabled = true;
+        evt.target.parentElement.classList.add('active-category');
+        serviceDescriptionsNode.querySelector('.description-' + newCategory).classList.remove('hidden');
+      }
+    });
+  }
 })();
