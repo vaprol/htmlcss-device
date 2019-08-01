8e83ad949774ef02340abab0d541f8cd3ebef7ba
Konstantin Pivnov <vaprol@gmail.com>
2019-08-01 11:31:48 +0300

    Скрипт переключения блоков в Услугах

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
 (function () {
+//  Строка поиска
+  var searchInput = document.querySelector('#header-search');
+  var searchButton = document.querySelector('.header-searchbar button');
+  var showSearchButton = function () {
+    searchButton.classList.remove('hidden');
+  };
+  var hideSearchButton = function () {
+    searchButton.classList.add('hidden');
+  };
+  searchInput.addEventListener('focus', showSearchButton);
+  searchInput.addEventListener('blur', hideSearchButton);
+  searchButton.addEventListener('mouseenter', function () {
+    searchInput.removeEventListener('blur', hideSearchButton);
+  });
+  searchButton.addEventListener('mouseleave', function () {
+    searchInput.addEventListener('blur', hideSearchButton);
+  });
+
 // Открытие навигационного подменю каталога
   var catalogOpener = document.querySelector('.catalog-opener');
   var subMenu = document.querySelector('.catalog-submenu');
@@ -69,6 +87,25 @@
     if (evt.target.nodeName === 'BUTTON') {
       clearInterval(sliderInterval);
       changeSlideManually(Array.from(sliderButtons).indexOf(evt.target) + 1);
+    }
+  });
+
+//  Работа блока услуг
+  var serviceButtonsNode = document.querySelector('.service-categories');
+  var serviceDescriptionsNode = document.querySelector('.service-descriptions');
+
+  serviceButtonsNode.addEventListener('click', function (evt) {
+    if (evt.target.nodeName === 'BUTTON') {
+      var oldCategory = serviceButtonsNode.querySelector('button[disabled]').classList[1].split('choose-').join('');
+      var newCategory = evt.target.classList[1].split('choose-').join('');
+
+      serviceButtonsNode.querySelector('button[disabled]').parentElement.classList.remove('active-category');
+      serviceButtonsNode.querySelector('button[disabled]').disabled = false;
+      serviceDescriptionsNode.querySelector('.description-' + oldCategory).classList.add('hidden');
+
+      evt.target.disabled = true;
+      evt.target.parentElement.classList.add('active-category');
+      serviceDescriptionsNode.querySelector('.description-' + newCategory).classList.remove('hidden');
     }
   })
 })();
